f2b093f4a0ed70fa9a099efad93aaf5563d908b0
gustiirf <rafligustisaputra123@gmail.com>
2024-10-03 12:38:11 +0700

    Music Organizer

---
+++ main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+        MusicManager manager = new MusicManager();
+
+        manager.addTrack("About You", "The 1975");
+        manager.addTrack("the stars", "Chelsea Cutler");
+        manager.addTrack("Moments", "One Direction");
+
+        manager.listAllTracks();
+
+        manager.playTrack(2);
+
+        manager.stopTrack();
+
+        manager.stopTrack();
+
+        System.out.println("Total number of tracks: " + manager.getTrackCount());
+    }
+}
